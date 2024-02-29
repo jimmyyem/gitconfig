0b7539bdb7cc55db877309131799422d7acca04f
jimmyyem <jimmyyem@qq.com>
2024-02-29 14:12:21 +0800

    fix:init v1

---
+++ index.js
@@ -0,0 +1,20 @@
+// example.js
+
+import { add, sub, addOneDay, sleep } from './lib/functions.js';
+import { Person } from './lib/person.js';
+import { getNasDaqData } from './lib/nasdaq.js';
+
+console.log(add(1,2), sub(1,2));
+
+const person = new Person("kaka", 22);
+console.log(person.toString());
+
+await sleep(1000);
+
+const today = new Date();
+console.log(today.toISOString().split('T')[0], addOneDay(today));
+
+await sleep(1000);
+
+const nasdaqData = await getNasDaqData(today.toISOString().split('T')[0]);
+console.log(nasdaqData);
---
+++ regex.js
@@ -0,0 +1,28 @@
+// regex.js
+
+const dateString = 'Jan, Feb 1, 2024';
+
+const [, month, day, year] = dateString.match(/(\w+) (\d+), (\d+)/);
+
+console.log(month, day, year);
+
+const monthMapping = {
+    Jan: "01",
+    Feb: "02",
+    Mar: "03",
+    Apr: "04",
+    May: "05",
+    Jun: "06",
+    Jul: "07",
+    Aug: "08",
+    Sep: "09",
+    Oct: "10",
+    Nov: "11",
+    Dec: "12",
+};
+
+const paddedDay = day.length == 1 ? `0${day}` : day;
+const paddedMonth = monthMapping[month];
+
+console.log(monthMapping[month]);
+console.log(`${year}-${paddedMonth}-${paddedDay}`);
