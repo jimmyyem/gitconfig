12e18f5c9bc42b6d65e6e67a8369a4e913795996
jimmyyem <jimmyyem@qq.com>
2024-02-29 14:20:39 +0800

    fix:c2

--- regex.js
+++ regex.js
@@ -2,27 +2,29 @@
 
 const dateString = 'Jan, Feb 1, 2024';
 
-const [, month, day, year] = dateString.match(/(\w+) (\d+), (\d+)/);
+function formatDate(dateString) {
+    const [, month, day, year] = dateString.match(/(\w+) (\d+), (\d+)/);
 
-console.log(month, day, year);
+    console.log(month, day, year);
 
-const monthMapping = {
-    Jan: "01",
-    Feb: "02",
-    Mar: "03",
-    Apr: "04",
-    May: "05",
-    Jun: "06",
-    Jul: "07",
-    Aug: "08",
-    Sep: "09",
-    Oct: "10",
-    Nov: "11",
-    Dec: "12",
-};
+    const monthMapping = {
+        Jan: "01",
+        Feb: "02",
+        Mar: "03",
+        Apr: "04",
+        May: "05",
+        Jun: "06",
+        Jul: "07",
+        Aug: "08",
+        Sep: "09",
+        Oct: "10",
+        Nov: "11",
+        Dec: "12",
+    };
 
-const paddedDay = day.length == 1 ? `0${day}` : day;
-const paddedMonth = monthMapping[month];
+    const paddedDay = day.length == 1 ? `0${day}` : day;
+    const paddedMonth = monthMapping[month];
 
-console.log(monthMapping[month]);
-console.log(`${year}-${paddedMonth}-${paddedDay}`);
+    console.log(monthMapping[month]);
+    console.log(`${year}-${paddedMonth}-${paddedDay}`);
+}
